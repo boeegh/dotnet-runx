ff4ff6dc665b1f101e45add1ca3b22092446070a
Sune Bøegh <sune@eksponent.com>
2023-03-04 18:51:54 +0100

    Test

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM mcr.microsoft.com/dotnet/aspnet:IMAGE_NO AS base
 
-RUN addgroup --gid 1737 -S dotnet &&\
-  adduser --uid 1737 -S dotnet -G dotnet &&\
-  apk add --no-cache dumb-init
+RUN case "IMAGE_NO" in \
+  *alpine*) addgroup --gid 1737 -S dotnet && adduser --uid 1737 -S dotnet -G dotnet && apk add --no-cache dumb-init ;; \
+  *      ) adduser --disabled-password --gecos "" --uid 1737 dotnet && apt-get update && apt-get install -y dumb-init && cat /etc/passwd ;; \
+  esac
 
 COPY start.sh /bin/start.sh
 RUN chmod +x /bin/start.sh
--- modify.sh
+++ modify.sh
@@ -1,5 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 IMAGE_VERSION_MARKER="IMAGE_NO"
+IMAGE_SET_VERSION=$1
 
-echo "Updating files using $IMAGE_VERSION_MARKER=$1"
-sed -i "s/$IMAGE_VERSION_MARKER/$1/" Dockerfile
+echo "Updating files using $IMAGE_VERSION_MARKER=$IMAGE_SET_VERSION"
+files=("Dockerfile")
+for fn in ${files[@]}; do
+  echo " - $fn"
+  cp -f "$fn" "$fn.bak"
+  sed -i "" "s/$IMAGE_VERSION_MARKER/$IMAGE_SET_VERSION/" "$fn"
+
+  if [[ $2 == "--build" ]]; then
+    docker build --progress plain --no-cache -t boeegh/dotnet-runx:dev . || echo "Docker build failed."
+  fi
+
+  mv -f "$fn.bak" "$fn"
+done
+
